d93dc4729213f0c72fb342419359e1341210dcb4
thafer1 <112597461+ZedZappie@users.noreply.github.com>
2022-11-16 22:44:37 -0500

    Cleaned up comments

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,19 +2,14 @@
 function renderLicenseBadge(data) {
   if (data == 'Apache'){
     return `![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)`;
-    //(https://opensource.org/licenses/Apache-2.0)
   } else if (data == 'GNU') {
     return `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`;
-    //(https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)
   } else if (data == 'MIT') {
     return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`;
-    //(https://opensource.org/licenses/MIT)
   } else if (data == 'BSD') {
     return `![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)`;
-    //(https://opensource.org/licenses/BSD-3-Clause)
   } else if (data == 'Boost') {
     return `![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)`;
-    //(https://www.boost.org/LICENSE_1_0.txt)
   } else {
     return '';
   }
@@ -36,10 +31,6 @@ function renderLicenseLink(data) {
       return '';
     }
 }
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
 
 // Function to generate markdown for README
 function generateMarkdown(data) {
